b9737249058367249503e52c66264dd85af8009a
guttman10 <guttman99@gmail.com>
2019-04-16 18:15:04 +0300

    up

--- src/ImageApp.js
+++ src/ImageApp.js
@@ -10,33 +10,93 @@ import { SearchBar } from 'react-native-elements';
 import Icon from "react-native-vector-icons/Ionicons";
 import { connect } from 'react-redux'
 class ImageApp extends Component {
-
+    state = {
+        search: '',
+        mode: 'gridview'
+      };
+      updateSearch = search => {
+        this.setState({ search });
+      };    
     Header(){
-        return(
-            <View style={[styles.header]}>
-            <View>
-              <Text style={[styles.textHeader]}>
-                Image Browser
-              </Text>
-              <TouchableOpacity style = {[styles.heartIcon]}>
-              <Icon
-                name={Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"}
-                color="#ccc"
-                size={40}
+        var headerScreen=[]
+        headerScreen.push(
+            <View >
+                <View style={[styles.header]}>
+                <Text style={[styles.textHeader]}>
+                    Image Browser
+                </Text>
+                <TouchableOpacity style = {[styles.heartIcon]}>
+                <Icon
+                    name={Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"}
+                    color="#ccc"
+                    size={40}
+                    />
+                </TouchableOpacity>
+                </View>
+                <View style = {[styles.searchStyle]}>
+                <SearchBar
+                    placeholder="Type Here..."
+                    onChangeText={this.updateSearch}
+                    value={this.state.search}
+                    lightTheme= {true}
                 />
-              </TouchableOpacity>
-            </View>
-            <View style = {[styles.searchStyle]}>
-            <SearchBar
-                placeholder="Type Here..."
-                onChangeText={()=>{}}
-                value={""}
-            />
-            </View>
+                </View>
             </View>
         )
+        headerScreen.push(this.Buttons())
+        return(headerScreen)
       }
-    
+
+    Buttons(){
+    if(this.state.mode == "gridview"){
+    return(
+        <View style = {[styles.touchableContainer]}>
+            <TouchableOpacity style = {[styles.viewButtonOn]}
+                onPress = {()=>{
+                    this.setState({
+                        mode : "gridview"
+                    })
+                }}
+            >
+                <Text style = {[styles.textViewButtonOn]}>Grid View</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style = {[styles.viewButtonOff]}
+                            onPress = {()=>{
+                                this.setState({
+                                    mode : "listview"
+                                })
+                            }}
+            >
+                <Text style = {[styles.textViewButtonOff]}>List View</Text>
+            </TouchableOpacity>
+        </View>
+        )
+    }
+    if(this.state.mode == "listview"){
+        return(
+            <View style = {[styles.touchableContainer]}>
+            <TouchableOpacity style = {[styles.viewButtonOff]}
+                onPress = {()=>{
+                    this.setState({
+                        mode : "gridview"
+                    })
+                }}
+            >
+                <Text style = {[styles.textViewButtonOff]}>Grid View</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style = {[styles.viewButtonOn]}
+                    onPress = {()=>{
+                        this.setState({
+                             mode : "listview"
+                       })
+                    }}
+            >
+                <Text style = {[styles.textViewButtonOn]}>List View</Text>
+            </TouchableOpacity>
+        </View>
+            )
+    }
+    }
 
     render() {
         return (
@@ -47,14 +107,14 @@ class ImageApp extends Component {
 
 function mapStateToProps(state) {
     return {
-        counter: state.counter
+        mode: state.mode
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        increaseCounter: () => dispatch({ type: 'INCREASE_COUNTER' }),
-        decreaseCounter: () => dispatch({ type: 'DECREASE_COUNTER' }),
+        viewMode: () => dispatch({ type: 'VIEW_MODE' }),
+        gridMode: () => dispatch({ type: 'GRID_MODE' }),
     }
 }
 
@@ -69,7 +129,7 @@ const styles = StyleSheet.create({
     },
     header :{
         backgroundColor: '#ff746d', 
-        flex: 0.1
+        height: 75  
     },
     textHeader: {
         color: "black",
@@ -83,9 +143,34 @@ const styles = StyleSheet.create({
         position: 'absolute'
     },
     searchStyle:{
-        position:'absolute',
         width:'100%',
-        top:'100%'
 
+    },
+    touchableContainer:{
+        width:'100%',
+        flexDirection: "row"
+    },
+    viewButtonOff:{
+        width:'50%',
+        height: 30,
+        alignItems: "center",
+        borderWidth: 1.5,
+        borderColor: '#0066ff',
+    },
+    viewButtonOn:{
+        width:'50%',
+        height: 30,
+        alignItems: "center",
+        borderWidth: 1.5,
+        borderColor: '#0066ff',
+        backgroundColor:'#0000ff'
+    },
+    textViewButtonOn:{
+        fontSize: 20,
+        color:'#ffffff'
+    },
+    textViewButtonOff:{
+        fontSize: 20,
+        color:'#000000'
     }
 });
